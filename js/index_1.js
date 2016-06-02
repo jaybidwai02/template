4df9f73b1a29f646fefa014c332e1cd78f80425d
Jay Bidwai <jay.bidwai@Infoedge.com>
2016-06-02 14:51:38 +0530

    adding changes for carousel

--- js/index_1.js
+++ js/index_1.js
@@ -1,66 +1,100 @@
 (function($){
 
 	$.fn.crousel = function(option){
-		console.log(this);
+		//console.log(this);
 		var $selector = this;
 		var $slider = $selector.find('.slider');
 		var $slide = $slider.find('.slide');
 
 
 		//setting options
-		var timeToSlide = option.timeToSlide;
+		var timeToSlide 		= option.timeToSlide;
+		var movingSpeed 		= option.movingSpeed;
+		var slideInterval 	= option.slideInterval;
+		var runOnLoad 			= option.runOnLoad;
+		var $triggerElement	= $('#'+option.triggerElement);
 
 		var totalSlides = $slide.length;
 		var slideWidth = $slide.width();
-		console.log($slide);
+		var numberOfSlides = $slide.length;
+		//console.log($slide.length);
 
 		//return this;
 
-		function start(){
-			alert(1234);
-		}
+		//animation variables to start and stop animation by clearInterval
+		var slideAnimationControl,isSlideAnimationStarted = false;
+		//start slide animation
+		function startSlideAnimaiton(){
+			isSlideAnimationStarted = true;
+			slideAnimationControl = setInterval(function(){
+				slideAnimation($selector, slideWidth, timeToSlide)
+			}, slideInterval);
+		};
+		//stop animation
+		function stopAnimation(animationControl){
+			clearInterval(animationControl);
+		};		
 
-		var moved = -500;
+		var moved = -slideWidth;
 		var counter = 1;
 
 		function slideAnimation(element, moveBy, duration){
-			var movePerLoop = moveBy / duration * 10; //amount of element to move in one loop
+			var movePerLoop = moveBy / duration * movingSpeed; //amount of element to move in one loop
 			//recursive funtion to move hole element by moverPerLoop
 			setTimeout(function(){
 				var sliderML = $slider.css('margin-left');
 				sliderML = parseInt(sliderML) - movePerLoop;
+
+				sliderML = sliderML < -slideWidth*counter ? -slideWidth*counter : sliderML; // if slide amount is > slide width reset to slide width
+				//console.log(sliderML);
+				$slider.css('margin-left',sliderML);
+				
 				//condition to check when one element is animated completly
-				if(sliderML < moved){
+			//	console.log(moved);
+				if(sliderML <= moved){
 					moved += sliderML;
 					counter++;
 					//condition to animate FIRST element after LAST element
-					if(counter==3){
-						moved = -500;
+					if(counter == numberOfSlides){
+						moved = -slideWidth;
 						counter = 1;
 						$slider.css('margin-left',0);
 					}
+					//clearInterval(slideAnimationControl);
 					return; //return after one element is animated completly
 				} 
 
-				$slider.css('margin-left',sliderML);
 
 				slideAnimation($selector, slideWidth - movePerLoop, timeToSlide - 10); //after on loop substract the amount of duration and amount of element animation
 
-			},10);
+			},movingSpeed);
 
-		}
+		};
+		//pause slide animation on mouse hover and start on mouseleave
+		$selector.on('mouseenter',function(){
+			stopAnimation(slideAnimationControl);
+		}).on('mouseleave',function(){
+			if(isSlideAnimationStarted) startSlideAnimaiton();
+		});
 
-	var slideAnimationControl = setInterval(function(){
-			slideAnimation($selector, slideWidth, timeToSlide)
-		}, 3000);
-		//slideIt($selector, slideWidth, timeToSlide);
+		//console.log(runOnLoad);
+		if(runOnLoad){
+			startSlideAnimaiton(); //start slide animation automatically
+		} 
+		else{
+			$triggerElement.on('click',startSlideAnimaiton);
+		} 
 	}
 
 })(jQuery);
 
 
 $("#myCraousel").crousel({
-	timeToSlide:1000
+	timeToSlide:1000,
+	movingSpeed:50,
+	slideInterval:3000,
+	runOnLoad:false,
+	triggerElement:'startIt'
 });
 
 /*function scrollTo(element, to, duration) {
